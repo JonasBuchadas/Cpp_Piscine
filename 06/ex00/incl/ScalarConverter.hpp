5516d2c2ce8e04e80a6a4318df39993067d465ee
JonasBuchadas <jonasmcbuchadas@gmail.com>
2023-08-01 08:57:46 +0100

    Template Functions

--- 06/ex00/incl/ScalarConverter.hpp
+++ 06/ex00/incl/ScalarConverter.hpp
@@ -6,22 +6,88 @@
 #include <limits>
 #include <climits>
 #include <cfloat>
-#include <IConverter.hpp>
+#include <AConverter.hpp>
 
 class ScalarConverter
 {
 private:
-	static long double	n;
-	// static int			i;
-	// static char			c;
-	// static float		f;
-	// static double		d;
-	void display();
-	void printChar();
-	void printInt();
-	void printFloat();
-	void printDouble();
+	template <typename T>
+	static void display(T n);
+	template <typename T>
+	static void printChar(T n);
+	template <typename T>
+	static void printInt(T n);
+	template <typename T>
+	static void printFloat(T n);
+	template <typename T>
+	static void printDouble(T n);
 
 public:
-	static void convert(char* str);
+	static void convert(char *str);
 };
+
+template <typename T>
+void ScalarConverter::display(T n)
+{
+	printChar(n);
+	printInt(n);
+	printFloat(n);
+	printDouble(n);
+}
+
+template <typename T>
+void ScalarConverter::printChar(T n)
+{
+	std::cout << "char: ";
+	if (n < std::numeric_limits<char>::min() || n > std::numeric_limits<char>::max())
+	{
+		std::cout << "impossible" << std::endl;
+		return;
+	}
+	char c = (char)n;
+	if (!isprint(c))
+	{
+		std::cout << "non displayable" << std::endl;
+		return;
+	}
+	std::cout << "'" << c << "'" << std::endl;
+}
+
+template <typename T>
+void ScalarConverter::printInt(T n)
+{
+	std::cout << "int: ";
+	if (n < std::numeric_limits<int>::min() || n > std::numeric_limits<int>::max())
+	{
+		std::cout << "impossible" << std::endl;
+		return;
+	}
+	int i = (int)n;
+	std::cout << i << std::endl;
+}
+
+template <typename T>
+void ScalarConverter::printFloat(T n)
+{
+	std::cout << "float: ";
+	if (n < -std::numeric_limits<float>::max() || n > std::numeric_limits<float>::max())
+	{
+		std::cout << "impossible" << std::endl;
+		return;
+	}
+	float f = (float)n;
+	std::cout << f << "f" << std::endl;
+}
+
+template <typename T>
+void ScalarConverter::printDouble(T n)
+{
+	std::cout << "double: ";
+	if (n < -std::numeric_limits<double>::max() || n > std::numeric_limits<double>::max())
+	{
+		std::cout << "impossible" << std::endl;
+		return;
+	}
+	double d = (double)n;
+	std::cout << d << std::endl;
+}
--- 06/ex00/srcs/ScalarConverter.cpp
+++ 06/ex00/srcs/ScalarConverter.cpp
@@ -1,68 +1,47 @@
 #include <ScalarConverter.hpp>
 
-long double ScalarConverter::n = 0;
+// long double ScalarConverter::n = 0;
 // int			ScalarConverter::i = 0;
 // char 		ScalarConverter::c = '\0';
 // float 		ScalarConverter::f = 0.0f;
 // double 		ScalarConverter::d = 0.0;
 
-void ScalarConverter::display() {
-  printChar();
-  printInt();
-  printFloat();
-  printDouble();
-}
-
-void ScalarConverter::printChar() {
-  std::cout << "char: ";
-  if (ScalarConverter::n < CHAR_MIN || ScalarConverter::n > CHAR_MAX) {
-    std::cout << "impossible" << std::endl;
-    return;
-  }
-  char c = (char)ScalarConverter::n;
-  if (!isprint(c)) {
-    std::cout << "non displayable" << std::endl;
-    return;
-  }
-  std::cout << "'" << c << "'" << std::endl;
-}
-
-void ScalarConverter::printInt() {
-  std::cout << "int: ";
-  if (ScalarConverter::n < INT_MIN || ScalarConverter::n > INT_MAX) {
-    std::cout << "impossible" << std::endl;
-    return;
-  }
-  int i = (int)ScalarConverter::n;
-  std::cout << i << std::endl;
-}
-
-void ScalarConverter::printFloat() {
-  std::cout << "float: ";
-  if (ScalarConverter::n < FLT_MIN || ScalarConverter::n > FLT_MAX) {
-    std::cout << "impossible" << std::endl;
-    return;
-  }
-  float f = (float)ScalarConverter::n;
-  std::cout << f << std::endl;
-}
-
-void ScalarConverter::printDouble() {
-  std::cout << "double: ";
-  if (ScalarConverter::n < DBL_MIN || ScalarConverter::n > DBL_MAX) {
-    std::cout << "impossible" << std::endl;
-    return;
-  }
-  double d = (double)ScalarConverter::n;
-  std::cout << d << std::endl;
-}
+// template <typename T>
+// void ScalarConverter::display(T) {
+// 	// printChar();
+// 	printInt();
+// 	printFloat();
+// 	printDouble();
+// }
 
 void ScalarConverter::convert(char *str) {
-  char *end;
-  // ScalarConverter::n = atol(str);
-  // std::string::size_type sz;
-  double d = strtod(str, &end);
-  std::cout << "str:\t" << d << std::endl;
-  std::cout << "end:\t" << end << std::endl;
+	// int		i;
+	// float	f;
+	// double	d;
+  	char*	end;
+	double	n;
+//   // ScalarConverter::n = atol(str);
+//   // std::string::size_type sz;
+	n = strtod(str, &end);
+	std::cout << "str:\t" << str << std::endl;
+	std::cout << "end:\t" << end << std::endl;
+	if (n > std::numeric_limits<char>::min() && n < std::numeric_limits<char>::max() && end[0] == '\0')
+	{
+		char c = atoi(str);
+		display(c);
+		return;
+	}
+	if (n > std::numeric_limits<int>::min() && n < std::numeric_limits<int>::max() && end[0] == '\0')
+	{
+		int i = atoi(str);
+		display(i);
+		return;
+	}
+	if (n > -std::numeric_limits<float>::max() && n < std::numeric_limits<int>::max())
+	{
+		float f = n;
+		display(f);
+		return;
+	}
   // display();
 }
